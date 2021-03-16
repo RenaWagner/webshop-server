19334ca9a648e7d34be002f8e8b40a9c49087f12
Rena Suzuki Wagner <rena.suzuki.wagner@gmail.com>
2021-03-16 17:41:11 +0100

    Added new router ProductCategories

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const userRouter = require("./routers/user");
 const orderRouter = require("./routers/order");
 const middleware = require("./auth/middleware");
 const adminRouter = require("./routers/admin");
+const productCategoryRouter = require("./routers/productCategory");
 
 app.use("/products", productRouter);
 app.use("/categories", categoryRouter);
@@ -22,5 +23,6 @@ app.use("/auth", authRouter);
 app.use("/users", userRouter);
 app.use("/orders", middleware.auth, orderRouter);
 app.use("/admin", middleware.authAdmin, adminRouter);
+app.use("/productCategories", productCategoryRouter);
 
 app.listen(PORT, () => console.log("Listening..."));
