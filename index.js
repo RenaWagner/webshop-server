b0abf0b21850ed7271573da39452c24e0a5d5104
Rena Suzuki Wagner <rena.suzuki.wagner@gmail.com>
2021-03-17 08:24:48 +0100

    App crashed

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const userRouter = require("./routers/user");
 const orderRouter = require("./routers/order");
 const middleware = require("./auth/middleware");
 const adminRouter = require("./routers/admin");
-const productCategoryRouter = require("./routers/productCategory");
 
 app.use("/products", productRouter);
 app.use("/categories", categoryRouter);
@@ -23,6 +22,5 @@ app.use("/auth", authRouter);
 app.use("/users", userRouter);
 app.use("/orders", middleware.auth, orderRouter);
 app.use("/admin", middleware.authAdmin, adminRouter);
-app.use("/productCategories", productCategoryRouter);
 
 app.listen(PORT, () => console.log("Listening..."));
