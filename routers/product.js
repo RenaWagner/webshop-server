344ed87acb562160bf8b73b7357129b8ceac888f
Rena Suzuki Wagner <rena.suzuki.wagner@gmail.com>
2021-03-17 08:13:21 +0100

    Added categories in the product get endpoint

--- routers/product.js
+++ routers/product.js
@@ -1,12 +1,17 @@
 const { Router } = require("express");
 const router = new Router();
 const Product = require("../models").product;
+const Category = require("../models").category;
 
 router.get("/", async (req, res, next) => {
   try {
     const limit = Math.min(req.query.limit || 25, 500); // limit indicates how many results are on a page.
     const offset = req.query.offset || 0;
-    const allProducts = await Product.findAll({ limit, offset });
+    const allProducts = await Product.findAll({
+      limit,
+      offset,
+      include: [Category],
+    });
     if (!allProducts) {
       res.status(404).send("Products not found");
     } else {
